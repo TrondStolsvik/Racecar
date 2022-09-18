e2c33af0d70ff48e8b669bafd072501a48dd91aa
TrondStolsvik <trondstolsvik@gmail.com>
2022-09-18 12:41:16 +0200

    pseudo code done
    fixed some html as well

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,30 @@
 function getString() {
-  //Declare a variable palindromeInput and store the input from the dom
-  //Call palindromeCheck and pass it the variable palindromeInput and store it's data in a new variable called palindromeChecked
-  //Call palindromeDisplay and pass it the variable palindromeChecked to display the data in the dom
+  //Clear the html
+  //Add "invisible" class to the results id to hide it before the start
+  //Declare a variable palInput and store the input from the dom
+  //Call stringReverse with the parameter palInput and store it in a new variable called revInput
+  //Call palCheck and pass it the variables palInput and revInput
 }
 
-function palindromeCheck(palInput) {
-  //Store the data from the parameter palInput in a variable called palindromeInput
-  //Call stringReverse and pass it the parameter palindromeInput, store this in a variable called stringReversed
-  //For-loop using the data in palindromeInput
-  //Use an if-statement to check each index against the same index of stringReversed
-  //If they all match, return stringReversed
-  //Else return palindromeInput
-}
-
-function stringReverse(palindromeInput) {
+function stringReverse(palInput) {
   //For-loop through the string to reverse it
   //Push the indexes from the string into a new variable called revString, starting from the end of the string
   //Return revString
 }
 
-function palindromeDisplay(palindromeChecked) {
-  //
+function palCheck(palInput, revInput) {
+  //Declare a variable called palChecked to store the string `Your input is not a palindrome. It was reversed as: ${palinputCheck}`
+  //Declare a variable called revChecked to store the string `Your input is a palindrome. It was reversed as: ${revInputCheck}`
+  //Store the data from the parameter palInput in a variable called palInutCheck
+  //Store the data from the parameter revInput in a variable called revInputCheck
+  //Use an if-statement to check if palInputCheck is equal to revInputCheck (force lower case for check if needed)
+  //If they are a match push data to revChecked
+  //Return revChecked
+  //Else push data to palChecked
+  //return palChecked
+}
+
+function palDisplay(palCheck) {
+  //Remove the "invisible" class from the results id
+  //Inject the result on the page
 }
